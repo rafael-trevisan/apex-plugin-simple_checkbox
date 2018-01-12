a24a9aded0909cd99b3ff0cd433400ed138c29bd
Rafael Trevisan <rafael@trevis.ca>
2018-01-12 10:27:27 -0800

    Updating README

--- source/item_type_plugin_ca_trevis_apex_simple_checkbox.sql
+++ source/item_type_plugin_ca_trevis_apex_simple_checkbox.sql
@@ -123,7 +123,7 @@ wwv_flow_api.create_plugin(
 ,p_standard_attributes=>'VISIBLE:SOURCE:ELEMENT:ELEMENT_OPTION'
 ,p_substitute_attributes=>true
 ,p_subscribe_plugin_settings=>true
-,p_version_identifier=>'18.1.2'
+,p_version_identifier=>'18.1.3'
 ,p_about_url=>'https://github.com/rafael-trevisan/apex-plugin-simple_checkbox'
 ,p_files_version=>2
 );
