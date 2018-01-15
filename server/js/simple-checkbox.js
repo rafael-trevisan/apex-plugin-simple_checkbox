69b5f45124ec7639cf55d7d9bad1b1f032077ef3
Rafael Trevisan <rafael@trevis.ca>
2018-01-14 22:42:47 -0800

    Adding checked/unchecked attributes to the input element

--- server/js/simple-checkbox.js
+++ server/js/simple-checkbox.js
@@ -1,19 +1,22 @@
 function ca_trevis_apex_simple_checkbox(e, c) {
-  var b = apex.jQuery("#" + e),
-    a = apex.jQuery("#" + e + "_HIDDEN");
+  var b = apex.jQuery("#" + e);
+  var a = apex.jQuery("#" + e + "_HIDDEN");
 
   function d() {
     a.val((b.is(":checked") === true) ? c.checked : c.unchecked)
   }
+
   apex.jQuery("#" + e).change(d);
   apex.jQuery(document).bind("apexbeforepagesubmit", d);
+
   apex.widget.initPageItem(e, {
     setValue: function(f) {
       b.attr("checked", (f === c.checked));
-      d()
+      d();
     },
     getValue: function() {
       return a.val()
     }
-  })
+  });
+
 };
